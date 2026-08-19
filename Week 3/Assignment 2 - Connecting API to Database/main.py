6459b3df025f8a13bf55f418ced429eb78f3939c
syedabdullah1567 <syedabdullah1567@gmail.com>
2026-08-19 21:27:47 +0500

    Stage 0: create SQLite database

---
+++ Week 3/Assignment 2 - Connecting API to Database/main.py
@@ -0,0 +1,60 @@
+import sqlite3
+
+
+from fastapi import FastAPI
+
+con = sqlite3.connect("tasks.db")
+
+cur = con.cursor()
+
+# Creating the database tables
+
+# res = cur.execute('''
+#     DROP TABLE IF EXISTS tasks
+# ''')
+
+# res = cur.execute('''
+
+#     CREATE TABLE tasks(
+#     id INTEGER PRIMARY KEY,
+#     title TEXT,
+#     done BOOLEAN
+# )
+
+# ''')
+
+res = cur.execute("SELECT name FROM sqlite_master")
+print(res.fetchone())
+
+res = cur.execute('''
+    SELECT COUNT(*) FROM tasks
+''')
+
+if (res.fetchone()[0] == 0) :
+    
+
+    cur.execute(''' 
+        INSERT INTO tasks(title, done)
+        VALUES
+        ("Buy groceries", FALSE),
+        ("Read Sqlite3 docs", TRUE),
+        ("Complete Stage 0", FALSE)
+    ''')
+    con.commit()
+
+
+res = cur.execute('''
+    SELECT * FROM tasks
+''')
+
+print(res.fetchall())
+
+app = FastAPI()
+
+@app.get("/tasks")
+async def get_all_tasks():
+    res = cur.execute('''
+        SELECT * FROM tasks
+    ''')
+
+    return res.fetchall()  
